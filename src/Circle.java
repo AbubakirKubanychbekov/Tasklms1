7a4f80b9404027c14634810e6a817c213616d18e
user <kubanycbekovabubakir@gmail.com>
2023-05-11 12:46:03 +0600

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,14 @@
+public class Circle {
+    public static final double PI = 3.14159265358979323846;
+
+    public static double getArea(double radius) {
+        return PI * (radius * radius);
+    }
+
+    public static double getCircumference(double radius) {
+        return PI * 2 * radius;
+    }
+
+}
+
+
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        double radius = 5.0;
+        double area = Circle.getArea(radius);
+        double circumference = Circle.getCircumference(radius);
+        System.out.println("Area: " + area);
+        System.out.println("Circumference: " + circumference);
+    }
+    }
